9190f7d29db8c0b96387891ee2cdfff781463167
Maxwell Kaufman <maxkaufman1@gmail.com>
2024-04-16 11:38:54 -0600

    added const's, location and local storage check and event listener for search button

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
 apiKey = "a834cf7ed82fb052aad800e6a5068f43";
+const citySearchInput = document.querySelector("serch-input");
+const searchButton = document.getElementById("search-button");
+const currentWeather = document.getElementById("weather-info");
+const fiveDayForecast = document.getElementById("five-day-forecast");
+const cityList = document.getElementById("search-history");
+let city = null;
+let recentArray = [];
+
+firstLoad();
+localStorageForRecent();
+
+//event listener for search button
+searchButton.addEventListener("click", handleSearch);
